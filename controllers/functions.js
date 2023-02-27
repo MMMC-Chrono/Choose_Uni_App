b16a4966a981171b969783b3a1413ce5b396ca6d
mmmc_chrono <maymyamyacin@gmail.com>
2023-02-28 05:03:08 +0630

    get functions for page

--- controllers/functions.js
+++ controllers/functions.js
@@ -1,7 +1,17 @@
 const main_page = (req, res) => {
-    res.sendFile(process.cwd()+ '/views/index.html')
+    res.sendFile(process.cwd()+ '/asserts/index.html')
+}
+
+const add_page = (req, res) => {
+    res.sendFile(process.cwd() + '/asserts/add.html')
+}
+
+const list_page = (req, res) => {
+    res.sendFile(process.cwd() + '/asserts/list.html')
 }
 
 module.exports = {
-    main_page
+    main_page,
+    add_page,
+    list_page
 }
