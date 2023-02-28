c3239cf20baab8abb12df03663a1a844e76379f6
mmmc_chrono <maymyamyacin@gmail.com>
2023-02-28 09:41:38 +0630

    added create_doc function

--- controllers/functions.js
+++ controllers/functions.js
@@ -10,8 +10,14 @@ const list_page = (req, res) => {
     res.sendFile(process.cwd() + '/asserts/list.html')
 }
 
+const create_doc = (req, res) => {
+    console.log(req.body)
+    return res.send("Added successfully")
+}
+
 module.exports = {
     main_page,
     add_page,
-    list_page
+    list_page,
+    create_doc
 }
