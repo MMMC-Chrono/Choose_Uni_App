5c14616289549543378c2c961ab991a1fa5e7793
mmmc_chrono <maymyamyacin@gmail.com>
2023-03-01 08:12:18 +0630

    added create doc function

--- controllers/functions.js
+++ controllers/functions.js
@@ -1,3 +1,5 @@
+const Uni = require('./models')
+
 const main_page = (req, res) => {
     res.sendFile(process.cwd()+ '/asserts/index.html')
 }
@@ -10,9 +12,14 @@ const list_page = (req, res) => {
     res.sendFile(process.cwd() + '/asserts/list.html')
 }
 
-const create_doc = (req, res) => {
+const create_doc = async (req, res) => {
     console.log(req.body)
-    return res.send("Added successfully")
+    const { title, link, price, program, about, note, selection } = req.body;
+    const uni = await Uni.create(req.body)
+    if (uni) {
+        return res.sendFile(process.cwd() + '/asserts/successful.html')
+    }
+    return res.send("Something went wrong")
 }
 
 module.exports = {
