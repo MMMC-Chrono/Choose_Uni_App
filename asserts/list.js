5b54529e7890b7f6079f52d5166ff1eb96ecbc56
mmmc_chrono <maymyamyacin@gmail.com>
2023-03-02 09:56:04 +0630

    get uniList

--- asserts/list.js
+++ asserts/list.js
@@ -1,5 +1,9 @@
-function getAll(get) {
-    console.log(get)
-}
-
-module.exports = { getAll }
+$(document).ready(function() {
+    console.log('hi')
+    $.getJSON('/list/uni', function(data) {
+        // console.log(data.length)
+        let list = []
+        list = data
+        console.log(list)
+    })
+})
