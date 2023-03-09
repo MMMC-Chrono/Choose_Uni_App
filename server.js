c759c80f058de184121d757cc9181347ead66c8d
mmmc_chrono <maymyamyacin@gmail.com>
2023-03-09 09:28:47 +0630

    added delete function

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const {
     add_page,
     list_page,
     create_doc,
-    filter,
+    deleted,
     getAllUni } = require('./controllers/functions')
 
 
@@ -33,8 +33,10 @@ app.route('/add')
 
 app.route('/list')
    .get(list_page)
-   .post(filter)
 
+app.route('/deleted')
+   .post(deleted)
+   
 app.route('/list/uni')
    .get(getAllUni)
 
