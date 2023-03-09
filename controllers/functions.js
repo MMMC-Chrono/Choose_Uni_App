79595f0574f56bc1f72ad10a38116c36cd11a699
mmmc_chrono <maymyamyacin@gmail.com>
2023-03-09 09:32:21 +0630

    changed create_doc and added deleted function

--- controllers/functions.js
+++ controllers/functions.js
@@ -13,8 +13,11 @@ const list_page = async(req, res) => {
 }
 
 const create_doc = async (req, res) => {
-    console.log(req.body)
     const { title, link, price, program, about, note, selection } = req.body;
+    const find = await Uni.find({link})
+    if (find.length !== 0) {
+        return res.send(`Already added: ${title} with link: ${link}`)
+    }
     const uni = await Uni.create(req.body)
     if (uni) {
         return res.sendFile(process.cwd() + '/asserts/successful.html')
@@ -22,22 +25,29 @@ const create_doc = async (req, res) => {
     return res.send("Something went wrong")
 }
 
+const deleted = async(req, res) => {
+    const { delete_sch } = req.body
+    const target = await Uni.deleteOne({title: delete_sch})
+    if (target.deletedCount === 0) {
+        return res.send("invalid name")
+    } else if (target.deletedCount !== 0) {
+        return res.sendFile(process.cwd() + '/asserts/deleted.html')
+    }
+    return res.send("invalid input")
+}
+
 const getAllUni = async (req, res) => {
     const uni = await Uni.find()
+    uni.sort((a, b) => /[0-9]+/.exec(a.price) - /[0-9]+/.exec(b.price))
     return res.send(uni)
 }
 
-
-const filter = async(req, res) => {
-    console.log(req.body)
-
-}
 
 module.exports = {
     main_page,
     add_page,
     list_page,
     create_doc,
-    filter,
+    deleted,
     getAllUni
 }
